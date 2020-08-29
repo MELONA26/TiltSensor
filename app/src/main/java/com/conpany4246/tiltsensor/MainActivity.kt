1cda98e0687feca4f34adabe9f1cf9ca18f7260a
hwang <hwangnc@gmail.com>
2020-08-29 22:12:29 +0900

    Initial commit

---
+++ app/src/main/java/com/conpany4246/tiltsensor/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.conpany4246.tiltsensor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
